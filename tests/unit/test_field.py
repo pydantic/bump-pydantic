f71aa3315c87eefba847650fb967fce9180bdf95
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-22 13:23:15 +0200

    ✨ Make sure we only rename on `Field` from `pydantic` (#30)

--- tests/unit/test_field.py
+++ tests/unit/test_field.py
@@ -1,3 +1,4 @@
+import pytest
 from libcst.codemod import CodemodTest
 
 from bump_pydantic.codemods.field import FieldCodemod
@@ -10,15 +11,41 @@ class TestReplaceConfigCommand(CodemodTest):
 
     def test_field_rename(self) -> None:
         before = """
-        from pydantic import BaseModel
+        from pydantic import BaseModel, Field
 
         class Potato(BaseModel):
             potato: List[int] = Field(..., min_items=1, max_items=10)
         """
         after = """
-        from pydantic import BaseModel
+        from pydantic import BaseModel, Field
 
         class Potato(BaseModel):
             potato: List[int] = Field(..., min_length=1, max_length=10)
+        """
+        self.assertCodemod(before, after)
+
+    def test_noop(self) -> None:
+        code = """
+        from pydantic import BaseModel
+        from potato import Field
+
+        class Potato(BaseModel):
+            potato: List[int] = Field(..., max_items=1)
+        """
+        self.assertCodemod(code, code)
+
+    @pytest.mark.xfail(reason="Not implemented yet")
+    def test_field_rename_with_pydantic_import(self) -> None:
+        before = """
+        import pydantic
+
+        class Potato(pydantic.BaseModel):
+            potato: List[int] = pydantic.Field(..., min_items=1, max_items=10)
+        """
+        after = """
+        from pydantic import BaseModel, Field
+
+        class Potato(pydantic.BaseModel):
+            potato: List[int] = pydantic.Field(..., min_length=1, max_length=10)
         """
         self.assertCodemod(before, after)
