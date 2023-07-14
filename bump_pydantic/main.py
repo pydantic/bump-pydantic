fa50122df71a3ea6e804a0bdfcd9b491a859be81
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-07-14 14:21:49 -0400

    Update bump_pydantic/main.py
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- bump_pydantic/main.py
+++ bump_pydantic/main.py
@@ -29,9 +29,7 @@ entrypoint = functools.partial(app, windows_expand_args=False)
 P = ParamSpec("P")
 T = TypeVar("T")
 
-DEFAULT_IGNORES = [
-    ".venv/**",
-]
+DEFAULT_IGNORES = [".venv/**"]
 
 
 def version_callback(value: bool):
